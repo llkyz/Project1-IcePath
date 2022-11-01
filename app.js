7887d881fe07a2a3324bd02333ba87d272362d3a
llky <lesterlohky@gmail.com>
2022-11-02 01:22:01 +0800

    Update
    Map centered
    Player can move
    Collision works
    Added win screen
    Added background

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-tileset = "Game Boy GBC - Pokemon Gold Silver - Tileset Day.png";
-
-const tile1 = new Image();
-tile1.src = tileset;
-
-const tile2 = new Image();
-tile2.src = tileset;
-
 const puzzle1 = [
   [5, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 6],
   [2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 4],
@@ -102,7 +94,7 @@ const puzzle5 = [
   ];
 */
 const puzzleCollection = [puzzle1, puzzle2, puzzle3, puzzle4, puzzle5];
-const startingPos = [[14,13,1],[0,3,3],[],[],[]] // X coord, Y coord, facing direction
+const startingPos = [[14, 13, 1], [0, 3, 3], [], [], []]; // X coord, Y coord, facing direction
 
 /*
 0 = Ice
@@ -121,9 +113,8 @@ const directionArray = [
   "player-idle-down",
   "player-idle-up",
   "player-idle-left",
-  "player-idle-right"
-]
-
+  "player-idle-right",
+];
 
 const tilemap = [
   "ice",
@@ -138,18 +129,29 @@ const tilemap = [
   "floor-goal",
 ];
 
-let randomPuzzle = Math.floor(Math.random() * puzzleCollection.length)
-let currentPuzzle = puzzleCollection[randomPuzzle]
-let posOffsetX = Math.ceil(puzzleCollection[randomPuzzle][0].length / 2) * -80
-let posOffsetY = Math.ceil(puzzleCollection[randomPuzzle].length / 2) * -80
-let startingPosX = startingPos[randomPuzzle][0]
-let startingPosY = startingPos[randomPuzzle][1]
-let facingDirection = directionArray[startingPos[randomPuzzle][2]]
+let randomPuzzle = Math.floor(Math.random() * puzzleCollection.length);
+let currentPuzzle = puzzleCollection[randomPuzzle];
+let posOffsetX = ((puzzleCollection[randomPuzzle][0].length + 1) / 2) * -80;
+let posOffsetY = ((puzzleCollection[randomPuzzle].length + 1) / 2) * -80;
+let startingPosX = startingPos[randomPuzzle][0];
+let startingPosY = startingPos[randomPuzzle][1];
+let facingDirection = directionArray[startingPos[randomPuzzle][2]];
+let movementEnabled = 1;
+let win = 0;
+let currentPosPx = [
+  posOffsetX + (startingPosX + 1) * 80,
+  posOffsetY + (startingPosY + 1) * 80,
+];
+let currentPosArray = [startingPosX, startingPosY];
+
+// =========================================
+// Instantiate Puzzle and Player
+// =========================================
 
 for (y = puzzleCollection[randomPuzzle].length; y > 0; y--) {
   for (x = 1; x < puzzleCollection[randomPuzzle][0].length + 1; x++) {
-    const sub_li = $("<div>").css("left", `${posOffsetX + (x * 80)}px`);
-    sub_li.css("top", `${posOffsetY + (y * 80)}px`);
+    const sub_li = $("<div>").css("left", `${posOffsetX + x * 80}px`);
+    sub_li.css("top", `${posOffsetY + y * 80}px`);
     sub_li.addClass("tile");
     sub_li.addClass(tilemap[puzzleCollection[randomPuzzle][y - 1][x - 1]]);
     $("body").append(sub_li);
@@ -158,46 +160,188 @@ for (y = puzzleCollection[randomPuzzle].length; y > 0; y--) {
 
 const playerObject = $("<div>");
 playerObject.addClass("player");
-playerObject.attr('id', facingDirection)
-playerObject.css("left",  `${posOffsetX + ((startingPosX+1) * 80)}px`);
-playerObject.css("top", `${posOffsetY + ((startingPosY+1) * 80)}px`);
-$("body").append(playerObject)
+playerObject.attr("id", facingDirection);
+playerObject.css("left", `${currentPosPx[0]}px`);
+playerObject.css("top", `${currentPosPx[1]}px`);
+$("body").append(playerObject);
 
-let currentPosPx = [posOffsetX + (startingPosX+1 * 80), posOffsetY + (startingPosY+1 * 80)]
-let currentPosArray = [startingPosX, startingPosY]
+// =========================================
+// Execute win condition
+// =========================================
+
+const winScreen = () => {
+  movementEnabled = 0;
+  $blackout = $("<div>").addClass("blackout");
+  $blackout.css("width", `${currentPuzzle[0].length * 40}px`);
+  $blackout.css("height", `${currentPuzzle.length * 40}px`);
+  $("body").append($blackout);
+  $("body").append($("<div>").addClass("win-screen"));
+  $("body").append($("<div>").addClass("return-to-title"));
+  $("body").append($("<div>").addClass("new-game"));
+};
+
+// =========================================
+// Movement Functions
+// =========================================
+
+const goLeft = () => {
+  if (currentPosArray[0] - 1 > -1) {
+    let newPos = currentPuzzle[currentPosArray[1]][currentPosArray[0] - 1];
+    if (newPos === 0 || newPos === 8 || newPos === 9) {
+      currentPosPx[0] -= 80;
+      currentPosArray[0] -= 1;
+      $(".player").css("left", `${currentPosPx[0]}px`);
+    } else {
+      return 1;
+    }
+    if (newPos === 8) {
+      return 1;
+    }
+    if (newPos === 9) {
+      win = 1;
+      winScreen();
+      return 1;
+    }
+  } else {
+    return 1;
+  }
+};
+
+const goUp = () => {
+  if (currentPosArray[1] - 1 > -1) {
+    let newPos = currentPuzzle[currentPosArray[1] - 1][currentPosArray[0]];
+    if (newPos === 0 || newPos === 8 || newPos === 9) {
+      currentPosPx[1] -= 80;
+      currentPosArray[1] -= 1;
+      $(".player").css("top", `${currentPosPx[1]}px`);
+    } else {
+      return 1;
+    }
+    if (newPos === 8) {
+      return 1;
+    }
+    if (newPos === 9) {
+      win = 1;
+      winScreen();
+      return 1;
+    }
+  } else {
+    return 1;
+  }
+};
+
+const goRight = () => {
+  if (currentPosArray[0] + 1 < currentPuzzle[0].length) {
+    let newPos = currentPuzzle[currentPosArray[1]][currentPosArray[0] + 1];
+    if (newPos === 0 || newPos === 8 || newPos === 9) {
+      currentPosPx[0] += 80;
+      currentPosArray[0] += 1;
+      $(".player").css("left", `${currentPosPx[0]}px`);
+    } else {
+      return 1;
+    }
+    if (newPos === 8) {
+      return 1;
+    }
+    if (newPos === 9) {
+      win = 1;
+      winScreen();
+      return 1;
+    }
+  } else {
+    return 1;
+  }
+};
+
+const goDown = () => {
+  if (currentPosArray[1] + 1 < currentPuzzle.length) {
+    let newPos = currentPuzzle[currentPosArray[1] + 1][currentPosArray[0]];
+    if (newPos === 0 || newPos === 8 || newPos === 9) {
+      currentPosPx[1] += 80;
+      currentPosArray[1] += 1;
+      $(".player").css("top", `${currentPosPx[1]}px`);
+    } else {
+      return 1;
+    }
+    if (newPos === 8) {
+      return 1;
+    }
+    if (newPos === 9) {
+      win = 1;
+      winScreen();
+      return 1;
+    }
+  } else {
+    return 1;
+  }
+};
+
+// =========================================
+// Keypress Listener
+// =========================================
 
 $(document).keydown(function (e) {
-  if (e.which === 37 || e.which === 65) {
-    console.log("Left");
-    if (currentPosArray[0] - 1 > -1) {
-      let newPos = currentPuzzle[currentPosArray[1]][currentPosArray[0] - 1]
-      if (newPos === 0 || newPos === 8 || newPos === 9) {
-        currentPosPx[0] -= 80
-        currentPosArray[0] -= 1
-        $('.player').css("left",  `${currentPosPx[0]}px`);
-      }
+  if (movementEnabled === 1) {
+    if (e.which === 37 || e.which === 65) {
+      // Left
+      $(".player").removeAttr("id");
+      $(".player").attr("id", "player-move-left");
+      movementEnabled = 0;
+      let myInterval = setInterval(() => {
+        if (goLeft() === 1) {
+          clearInterval(myInterval);
+          if (win === 0) {
+            movementEnabled = 1;
+          }
+          $(".player").removeAttr("id");
+          $(".player").attr("id", "player-idle-left");
+        }
+      }, 50);
+    } else if (e.which === 38 || e.which === 87) {
+      // Up
+      $(".player").removeAttr("id");
+      $(".player").attr("id", "player-move-up");
+      movementEnabled = 0;
+      let myInterval = setInterval(() => {
+        if (goUp() === 1) {
+          clearInterval(myInterval);
+          if (win === 0) {
+            movementEnabled = 1;
+          }
+          $(".player").removeAttr("id");
+          $(".player").attr("id", "player-idle-up");
+        }
+      }, 50);
+    } else if (e.which === 39 || e.which === 68) {
+      // Right
+      $(".player").removeAttr("id");
+      $(".player").attr("id", "player-move-right");
+      movementEnabled = 0;
+      let myInterval = setInterval(() => {
+        if (goRight() === 1) {
+          clearInterval(myInterval);
+          if (win === 0) {
+            movementEnabled = 1;
+          }
+          $(".player").removeAttr("id");
+          $(".player").attr("id", "player-idle-right");
+        }
+      }, 50);
+    } else if (e.which === 40 || e.which === 83) {
+      // Down
+      $(".player").removeAttr("id");
+      $(".player").attr("id", "player-move-down");
+      movementEnabled = 0;
+      let myInterval = setInterval(() => {
+        if (goDown() === 1) {
+          clearInterval(myInterval);
+          if (win === 0) {
+            movementEnabled = 1;
+          }
+          $(".player").removeAttr("id");
+          $(".player").attr("id", "player-idle-down");
+        }
+      }, 50);
     }
-  } 
-  else if (e.which === 38 || e.which === 87) {
-    console.log("Up");
-    currentPosPx[1] -= 80
-    $('.player').css("top",  `${currentPosPx[1]}px`);
-  } 
-  else if (e.which === 39 || e.which === 68) {
-    console.log("Right");
-    if (currentPosArray[0] + 1 < currentPuzzle[0].length) {
-      let newPos = currentPuzzle[currentPosArray[1]][currentPosArray[0] + 1]
-      if (newPos === 0 || newPos === 8 || newPos === 9) {
-        currentPosPx[0] += 80
-        currentPosArray[0] += 1
-        $('.player').css("left",  `${currentPosPx[0]}px`);
-        console.log("currentPosArray: " + (currentPosArray[0]-1) + ":" + currentPosArray[1] + " to " + currentPosArray[0] + ":" + currentPosArray[1])
-      }
-    }
-  } 
-  else if (e.which === 40 || e.which === 83) {
-    console.log("Down");
-    currentPosPx[1] += 80
-    $('.player').css("top",  `${currentPosPx[1]}px`);
   }
 });
