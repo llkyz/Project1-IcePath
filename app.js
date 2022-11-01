02117d67adaa815b91ab42f1344c7538fb6653d2
llkyz <lesterlohky@gmail.com>
2022-11-01 17:09:04 +0800

    Update

--- app.js
+++ app.js
@@ -102,7 +102,8 @@ const puzzle5 = [
   ];
 */
 const puzzleCollection = [puzzle1, puzzle2, puzzle3, puzzle4, puzzle5];
-const startingPos = [[13,14,3],[],[],[],[]] // X coord, Y coord, facing direction
+const startingPos = [[14,13,1],[0,3,3],[],[],[]] // X coord, Y coord, facing direction
+
 /*
 0 = Ice
 1 = Rock
@@ -116,7 +117,7 @@ const startingPos = [[13,14,3],[],[],[],[]] // X coord, Y coord, facing directio
 9 = Floor Goal
 */
 
-const facingDirection = [
+const directionArray = [
   "player-idle-down",
   "player-idle-up",
   "player-idle-left",
@@ -138,23 +139,12 @@ const tilemap = [
 ];
 
 let randomPuzzle = Math.floor(Math.random() * puzzleCollection.length)
-posOffsetX = Math.ceil(puzzleCollection[randomPuzzle][0].length / 2) * -80
-posOffsetY = Math.ceil(puzzleCollection[randomPuzzle].length / 2) * -80
-//console.log(randomPuzzle)
-
-/*
-for (y = puzzleCollection[randomPuzzle].length; y > 0; y--) {
-  const sub_ul = $("<ul/>");
-  for (x = 1; x < puzzleCollection[randomPuzzle][0].length + 1; x++) {
-    const sub_li = $("<li/>").css("left", `${posOffsetX + (x * 40*2)}px`);
-    sub_li.css("top", `${posOffsetY + (y * 40)}px`);
-    sub_li.addClass("tile");
-    sub_li.addClass(tilemap[puzzleCollection[randomPuzzle][y - 1][x - 1]]);
-    sub_ul.append(sub_li);
-  }
-  $("body").append(sub_ul);
-}
-*/
+let currentPuzzle = puzzleCollection[randomPuzzle]
+let posOffsetX = Math.ceil(puzzleCollection[randomPuzzle][0].length / 2) * -80
+let posOffsetY = Math.ceil(puzzleCollection[randomPuzzle].length / 2) * -80
+let startingPosX = startingPos[randomPuzzle][0]
+let startingPosY = startingPos[randomPuzzle][1]
+let facingDirection = directionArray[startingPos[randomPuzzle][2]]
 
 for (y = puzzleCollection[randomPuzzle].length; y > 0; y--) {
   for (x = 1; x < puzzleCollection[randomPuzzle][0].length + 1; x++) {
@@ -168,20 +158,46 @@ for (y = puzzleCollection[randomPuzzle].length; y > 0; y--) {
 
 const playerObject = $("<div>");
 playerObject.addClass("player");
-playerObject.attr('id', "player-idle-left")
-playerObject.css("left", "40px");
-playerObject.css("top", `${40}px`);
+playerObject.attr('id', facingDirection)
+playerObject.css("left",  `${posOffsetX + ((startingPosX+1) * 80)}px`);
+playerObject.css("top", `${posOffsetY + ((startingPosY+1) * 80)}px`);
 $("body").append(playerObject)
 
+let currentPosPx = [posOffsetX + (startingPosX+1 * 80), posOffsetY + (startingPosY+1 * 80)]
+let currentPosArray = [startingPosX, startingPosY]
 
 $(document).keydown(function (e) {
   if (e.which === 37 || e.which === 65) {
     console.log("Left");
-  } else if (e.which === 38 || e.which === 87) {
+    if (currentPosArray[0] - 1 > -1) {
+      let newPos = currentPuzzle[currentPosArray[1]][currentPosArray[0] - 1]
+      if (newPos === 0 || newPos === 8 || newPos === 9) {
+        currentPosPx[0] -= 80
+        currentPosArray[0] -= 1
+        $('.player').css("left",  `${currentPosPx[0]}px`);
+      }
+    }
+  } 
+  else if (e.which === 38 || e.which === 87) {
     console.log("Up");
-  } else if (e.which === 39 || e.which === 68) {
+    currentPosPx[1] -= 80
+    $('.player').css("top",  `${currentPosPx[1]}px`);
+  } 
+  else if (e.which === 39 || e.which === 68) {
     console.log("Right");
-  } else if (e.which === 40 || e.which === 83) {
+    if (currentPosArray[0] + 1 < currentPuzzle[0].length) {
+      let newPos = currentPuzzle[currentPosArray[1]][currentPosArray[0] + 1]
+      if (newPos === 0 || newPos === 8 || newPos === 9) {
+        currentPosPx[0] += 80
+        currentPosArray[0] += 1
+        $('.player').css("left",  `${currentPosPx[0]}px`);
+        console.log("currentPosArray: " + (currentPosArray[0]-1) + ":" + currentPosArray[1] + " to " + currentPosArray[0] + ":" + currentPosArray[1])
+      }
+    }
+  } 
+  else if (e.which === 40 || e.which === 83) {
     console.log("Down");
+    currentPosPx[1] += 80
+    $('.player').css("top",  `${currentPosPx[1]}px`);
   }
 });
